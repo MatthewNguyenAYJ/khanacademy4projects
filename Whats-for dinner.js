6074a5ac6c422482f8a2ff89d9b3c7cc1eb6e8a5
MatthewNguyenAYJ <42983707+MatthewNguyenAYJ@users.noreply.github.com>
2018-10-02 14:01:57 -0400

    Create Whats-for dinner.js
    this from khan academy.

---
+++ Whats-for dinner.js
@@ -0,0 +1,25 @@
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300); 
+noStroke();
+//pizza
+fill(242, 183, 34);
+triangle(324,128,200,338,85,126);
+//crust
+fill(94, 62, 14);
+rect(80,122,251,33);
+//pepperoni
+fill(153, 20, 20);
+ellipse(151,188,38,38);
+ellipse(236,197,38,38);
+ellipse(194,269,32,28);
+ellipse(202,222,27,28);
+ellipse(199,312,26,20);
+//oilives 
+fill(48, 34, 34);
+ellipse(158,234,12,11);
+ellipse(200,247,8,11);
+ellipse(230,252,8,11);
+ellipse(267,175,11,11);
+ellipse(197,190,10,11);
+ellipse(197,293,10,11);
