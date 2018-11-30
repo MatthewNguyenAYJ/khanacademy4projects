572d5ef96d1853479e870177e53f8301dcb8ba96
MatthewNguyenAYJ <42983707+MatthewNguyenAYJ@users.noreply.github.com>
2018-11-30 00:17:38 -0500

    Create Memory-game.js

---
+++ Memory-game.js
@@ -0,0 +1,179 @@
+// Scroll down to "About" for instructions on this project ↓
+
+/*CHANGES: Added the number of tries in the display, added more more pictures in the game, change the picture of the tile, added 1 more row, made a cooler win state and after you win a game, you can click anywhere to restart. 
+*/
+
+var Tile = function(x, y, face) {
+    this.x = x;
+    this.y = y;
+    this.width = 70;
+    this.face = face;
+    this.isFaceUp = false;
+    this.isMatch = false;
+};
+
+Tile.prototype.draw = function() {
+    fill(214, 247, 202);
+    strokeWeight(2);
+    rect(this.x, this.y, this.width, this.width, 10);
+    if (this.isFaceUp) {
+        image(this.face, this.x, this.y, this.width, this.width);
+    } else {
+        image(getImage("avatars/avatar-team"), this.x, this.y, this.width, this.width);
+    }
+};
+
+Tile.prototype.isUnderMouse = function(x, y) {
+    return x >= this.x && x <= this.x + this.width  &&
+        y >= this.y && y <= this.y + this.width;
+};
+
+// Global config
+var NUM_COLS = 5;
+var NUM_ROWS = 5;
+
+// Declare an array of all possible faces
+var faces = [
+    getImage("avatars/leafers-seed"),
+    getImage("avatars/leafers-seedling"),
+    getImage("avatars/aqualine-sapling"),
+    getImage("avatars/duskpin-ultimate"),
+    getImage("avatars/orange-juice-squid"),
+    getImage("avatars/leafers-tree"),
+    getImage("avatars/leafers-ultimate"),
+    getImage("avatars/marcimus"),
+    getImage("avatars/mr-pants"),
+    getImage("avatars/mr-pink"),
+    getImage("avatars/old-spice-man"),
+    getImage("avatars/robot_female_1")
+];
+
+// Make an array which has 2 of each, then randomize it
+var possibleFaces = faces.slice(0);
+var selected = [];
+for (var i = 0; i < (NUM_COLS * NUM_ROWS) / 2; i++) {
+    // Randomly pick one from the array of remaining faces
+    var randomInd = floor(random(possibleFaces.length));
+    var face = possibleFaces[randomInd];
+    // Push twice onto array
+    selected.push(face);
+    selected.push(face);
+    // Remove from array
+    possibleFaces.splice(randomInd, 1);
+}
+
+// Now shuffle the elements of that array
+var shuffleArray = function(array) {
+    var counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        var ind = Math.floor(Math.random() * counter);
+        // Decrease counter by 1
+        counter--;
+        // And swap the last element with it
+        var temp = array[counter];
+        array[counter] = array[ind];
+        array[ind] = temp;
+    }
+};
+shuffleArray(selected);
+
+// Create the tiles
+var tiles = [];
+for (var i = 0; i < NUM_COLS; i++) {
+    for (var j = 0; j < NUM_ROWS; j++) {
+        var tileX = i * 80 + 4;
+        var tileY = j * 76 + 21;
+        var tileFace = selected.pop();
+        tiles.push(new Tile(tileX, tileY, tileFace));
+    }
+}
+
+
+background(255, 255, 255);
+
+var numTries = 0;
+var numMatches = 0;
+var flippedTiles = [];
+var delayStartFC = null;
+
+mouseClicked = function() {
+    for (var i = 0; i < tiles.length; i++) {
+        var tile = tiles[i];
+        if (tile.isUnderMouse(mouseX, mouseY)) {
+            if (flippedTiles.length < 2 && !tile.isFaceUp) {
+                tile.isFaceUp = true;
+                flippedTiles.push(tile);
+                if (flippedTiles.length === 2) {
+                    numTries++;
+                    if (flippedTiles[0].face === flippedTiles[1].face) {
+                        flippedTiles[0].isMatch = true;
+                        flippedTiles[1].isMatch = true;
+                        flippedTiles.length = 0;
+                        numMatches++;
+                    }
+                    delayStartFC = frameCount;
+                }
+            } 
+            loop();
+        }
+    }
+};
+
+
+
+draw = function() {
+    background(255, 255, 255);
+    if (delayStartFC && (frameCount - delayStartFC) > 30) {
+        for (var i = 0; i < tiles.length; i++) {
+            var tile = tiles[i];
+            if (!tile.isMatch) {
+                tile.isFaceUp = false;
+            }
+        }
+        flippedTiles = [];
+        delayStartFC = null;
+        noLoop();
+    }
+    
+    for (var i = 0; i < tiles.length; i++) {
+        tiles[i].draw();
+    }
+    
+    textSize(20);
+    fill(20, 20, 20);
+    text('# of tries: '+numTries, 31,16);
+    
+    
+    if (numMatches === tiles.length/2) {
+        background(255, 0, 0);
+        fill(0, 0, 0);
+        textSize(20);
+        
+        
+        text("You found them all in " + numTries + " tries!", 20, 375);
+        fill(0, 21, 255);
+        text ("Click anywhere to restart", 105, 200);
+        
+     
+       mouseClicked = function(){
+        Program.restart();
+        };
+    
+     mouseMoved = function(){
+        fill(119, 227, 113);
+        image(getImage("creatures/Hopper-Happy"),mouseX, mouseY, 40, 40);
+        };
+    
+    
+    }
+  
+   
+    
+    
+};
+
+noLoop();
+
